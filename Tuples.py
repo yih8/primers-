aadb7806bd5935e9d14e073b24a17a56f3ea4e2d
yih8 <80453336+yih8@users.noreply.github.com>
2022-06-30 00:14:50 +0200

    Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,10 @@
+my_tuple = (1,'banana',())
+a=my_tuple[0]
+b=my_tuple[1]
+c=my_tuple[-1]
+d=my_tuple[1:2]
+e=my_tuple[1:]
+f=my_tuple[:1]
+g=my_tuple[:-1]
+
+print(a,"   ",b,"   ",c,"   ",d,"   ",e,"   ",f,"   ",g)
